029fcafb0538fbc4645fb0f2104e98d623d976df
Seikovich Seika <elagalya@gmail.com>
2025-10-24 00:10:43 +0300

    first try

--- elagin.sergey/T2/main.cpp
+++ elagin.sergey/T2/main.cpp
@@ -11,14 +11,14 @@ int main()
   using input_it_t = std::istream_iterator<DataStruct>;
   using output_it_t = std::ostream_iterator<DataStruct>;
 
-  std::vector<DataStruct> data;
+  std::vector<DataStruct> sdata;
 
   while (!std::cin.eof()) {
     if (std::cin.fail()) {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    std::copy(input_it_t{std::cin}, input_it_t{}, std::back_inserter(data));
+    std::copy(input_it_t{std::cin}, input_it_t{}, std::back_inserter(sdata));
   }
 
   if (std::cin.bad()) {
@@ -26,8 +26,8 @@ int main()
     return 1;
   }
 
-  std::sort(data.begin(), data.end());
-  std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
+  std::sort(sdata.begin(), sdata.end());
+  std::copy(sdata.cbegin(), sdata.cend(), output_it_t{std::cout, "\n"});
 
   return 0;
 }
